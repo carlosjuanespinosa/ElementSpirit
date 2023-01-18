8da30a8bfc410f26870ff59fb49addfa6783b9fc
carlosjuanespinosa <carlosjuanespinosa@gmail.com>
2023-01-18 21:00:14 +0100

    save

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
